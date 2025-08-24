30843cb3e0f81ace4e780578b2dabe6b30b4b26d
Sujanqwe <dutsujan@gmail.com>
2025-08-25 01:24:11 +0530

    123awd

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,14 +1,14 @@
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
-
+import { Colors } from "react-native-ui-lib";
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { Slot } from 'expo-router'
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <SafeAreaView style={{ flex: 1 }} edges={['top'] }>
+      <SafeAreaView style={{ flex: 1, backgroundColor:Colors.background }} edges={['top'] }>
         <Slot />
       </SafeAreaView>
     </ClerkProvider>
